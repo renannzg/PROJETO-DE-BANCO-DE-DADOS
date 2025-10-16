804f5fa09844a1831bae47f7e88f615903a54311
Renan Miguel Zanetti Gatti <miguelrenan618@gmail.com>
2025-10-16 11:14:07 -0300

    Add Tarefa class with initialization and string method

--- src/model/tarefa.py
+++ src/model/tarefa.py
@@ -1,1 +1,13 @@
+class Tarefa:
+    def __init__(self, id_tarefa=None, titulo=None, descricao=None, data_criacao=None, data_limite=None, status=None, id_usuario=None):
+        self.id_tarefa = id_tarefa
+        self.titulo = titulo
+        self.descricao = descricao
+        self.data_criacao = data_criacao
+        self.data_limite = data_limite
+        self.status = status
+        self.id_usuario = id_usuario
+
+    def __str__(self):
+        return f"{self.id_tarefa} - {self.titulo} [{self.status}] - Usuário: {self.id_usuario}"
 
