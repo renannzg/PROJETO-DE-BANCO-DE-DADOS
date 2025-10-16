3c66aef5712a705c8c3e4f20819d7e408c3d447e
Renan Miguel Zanetti Gatti <miguelrenan618@gmail.com>
2025-10-16 11:13:39 -0300

    Add splash function to display project information

--- src/utils/splash_screen.py
+++ src/utils/splash_screen.py
@@ -1,1 +1,10 @@
+def splash(names):
+    print("==============================")
+    print("SISTEMA DE GERENCIAMENTO DE TAREFAS")
+    print("Disciplina: Banco de Dados")
+    print("Professor: Howard Roatti")
+    print("Componentes do grupo:")
+    for n in names:
+        print(" -", n)
+    print("==============================")
 
