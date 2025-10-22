d25d9f3ad3f65f00b91d0daf5b4fdfff46edcf3d
Renan Miguel Zanetti Gatti <miguelrenan618@gmail.com>
2025-10-22 18:49:02 -0300

    Delete src/model directory

--- src/model/usuario.py
+++
@@ -1,9 +0,0 @@
-class Usuario:
-    def __init__(self, id_usuario=None, nome=None, email=None):
-        self.id_usuario = id_usuario
-        self.nome = nome
-        self.email = email
-
-    def __str__(self):
-        return f"{self.id_usuario} - {self.nome} ({self.email})"
-
